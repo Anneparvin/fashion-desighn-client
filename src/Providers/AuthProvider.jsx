92e64faeea0bfb35b8c68dd4cc93a086d26452b8
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-07-06 21:07:44 +0600

    add6

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { app } from '../firebase/firebase.config';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
 
-const AuthProvider = () => {
+const AuthContext = createContext(null);
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+     
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider)
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+          });
+    }
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        googleSignIn,
+        logOut,
+        updateUserProfile
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtAVSmdriy-8JhZ3qZXIOqs1etHcTwtdU",
+  authDomain: "fashionmaster-64d4f.firebaseapp.com",
+  projectId: "fashionmaster-64d4f",
+  storageBucket: "fashionmaster-64d4f.appspot.com",
+  messagingSenderId: "231452534300",
+  appId: "1:231452534300:web:c2ed3ec6ff4f7aeb4161d9",
+  measurementId: "G-YXJVSK10XN"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
